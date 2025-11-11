24f91420104e67383a00ed108a3f7a6f41536660
William Guzman <125222355+williamG7@users.noreply.github.com>
2025-11-11 02:04:12 +0100

    commit reto E

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
+def on_forever():
+    temp = input.temperature()
+    led.plot_bar_graph(temp, 50)
+basic.forever(on_forever)
 
+x = 2
+y = 2
+
+def on_forever2():
+    global x, y
+
+    led.plot(x, y)
+    basic.pause(50)
+    led.unplot(x, y)
+
+    aceleracionX = input.acceleration(Dimension.X)
+    aceleracionY = input.acceleration(Dimension.Y)
+    if aceleracionX <= 150 and x > 0:
+        x = x - 1
+    if aceleracionX > 150 and x < 4:
+        x = x + 1
+    if aceleracionY <= 150 and y > 0:
+        y = y - 1
+    if aceleracionY > 150 and y < 4:
+       y = y + 1
+ 
+basic.forever(on_forever2)
--- main.ts
+++ main.ts
@@ -1,1 +1,30 @@
-
+basic.forever(function on_forever() {
+    let temp = input.temperature()
+    led.plotBarGraph(temp, 50)
+})
+let x = 2
+let y = 2
+basic.forever(function on_forever2() {
+    
+    led.plot(x, y)
+    basic.pause(50)
+    led.unplot(x, y)
+    let aceleracionX = input.acceleration(Dimension.X)
+    let aceleracionY = input.acceleration(Dimension.Y)
+    if (aceleracionX <= 150 && x > 0) {
+        x = x - 1
+    }
+    
+    if (aceleracionX > 150 && x < 4) {
+        x = x + 1
+    }
+    
+    if (aceleracionY <= 150 && y > 0) {
+        y = y - 1
+    }
+    
+    if (aceleracionY > 150 && y < 4) {
+        y = y + 1
+    }
+    
+})
